334c737f6e69bc21389e52c1941938f8cb6cf089
Karan Adwani <karanadwani.official@gmail.com>
2023-07-18 15:54:02 +0530

    hex.js file completed

--- 1-color-flipper/hex.js
+++ 1-color-flipper/hex.js
@@ -1,19 +1,22 @@
-const hexColors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const button = document.getElementById("btn");
-const color = document.getElementsByClassName("color");
+const hexCol = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.getElementById('btn');
+const color = document.querySelector(".color");
 
 button.addEventListener('click', ()=>{
-    let hexC = "#";
-
-    for(let i = 0;i<6;i++)
+    // creating a random for accessing the colors array.
+    let hex = "#";
+    let i = 0;
+    while(i<6)
     {
-        hexC += hexColors[getRandomNumber()];
+        let randomNumber = getRandomNumber();
+        hex = hex + hexCol[ randomNumber ];
+        i++;
     }
-    
-    color.textContent = hexC;
-    document.body.style.backgroundColor = hexC;
+
+    color.innerHTML = hex;
+    document.body.style.backgroundColor = hex;
 });
 
-let getRandomNumber = () => {
-    return Math.floor( Math.random * hexColors.length );
-}
+let getRandomNumber = ()=>{
+    return Math.floor((Math.random() * hexCol.length ))
+};
