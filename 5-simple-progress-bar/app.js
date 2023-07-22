98fbde0152590a1ef116ec402d61af42388e51a5
Karan Adwani <karanadwani.official@gmail.com>
2023-07-22 22:13:30 +0530

    app.js completed

---
+++ 5-simple-progress-bar/app.js
@@ -0,0 +1,6 @@
+const start = document.querySelector("#start");
+const progressBar = document.querySelector(".progress-bar");
+
+const progressAnimation = () => {
+    progressBar.classList.add("progress");
+}
